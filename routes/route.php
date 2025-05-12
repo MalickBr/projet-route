3bfd0b3e4a253f8c9404d5452f3a6eec806f72f4
malick <malickfowoue@gmail.com>
2025-05-12 17:31:53 +0200

    third commit

--- routes/route.php
+++ routes/route.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
-       <li><a href="page=home">Acceuil</a></li>
-       <li><a href="page=contact">A propos</a></li>
-       <li><a href="page=about">Contact</a></li> 
+       <li><a href="?page=home">Acceuil</a></li>
+       <li><a href="?page=contact">A propos</a></li>
+       <li><a href="?page=about">Contact</a></li> 
     </ul>
 </nav>
