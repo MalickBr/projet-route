3480c76e75dc122a26605d290f0cfcc9b988e227
malick <malickfowoue@gmail.com>
2025-05-12 15:54:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les routes</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <?php
+         include_once __DIR__ .'/routes/routes.php';
+    ?>
+
+    <main>
+       <!-- <?php include_once __DIR__ .'/pages/$page.php' ?> -->
+    </main>
+
+    <footer>
+        <p>&copy; - PHP -2025</php>
+    </footer>
+</body>
+</html>
